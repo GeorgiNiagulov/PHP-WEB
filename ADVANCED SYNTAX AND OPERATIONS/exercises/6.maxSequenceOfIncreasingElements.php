14283877342d5c978de43844a9ac09881a978bb1
Georgi Niagulov <goshiniokrump@gmail.com>
2019-05-18 13:59:15 +0300

    fixed max sequence of increasing elements

--- ADVANCED SYNTAX AND OPERATIONS/exercises/6.maxSequenceOfIncreasingElements.php
+++ ADVANCED SYNTAX AND OPERATIONS/exercises/6.maxSequenceOfIncreasingElements.php
@@ -8,7 +8,7 @@ $startIndex = 0;
 for ($row=0; $row < count($seq); $row++) { 
   $currentCount = 0;
   for ($col=$row; $col < count($seq)-1; $col++) { 
-    if ($seq[$row] < $seq[$col+1]) {
+    if ($seq[$col] < $seq[$col+1]) {
       $currentCount++;
       if ($currentCount > $maxCount) {
         $maxCount = $currentCount;
