2f47e5bc977d83ba744a3218aaaecca01ed14b1d
Georgi Niagulov <goshiniokrump@gmail.com>
2019-05-19 18:29:13 +0300

    added printMonth

--- ADVANCED SYNTAX AND OPERATIONS/lab/1.printMonth.php
+++ ADVANCED SYNTAX AND OPERATIONS/lab/1.printMonth.php
@@ -1,3 +1,11 @@
 <?php
 
+$selectedMonth = intval(readline());
+
 $months = ["January", "February", "March", "April", "May", "June",  "July", "August", "September", "October", "November", "December"];
+
+foreach ($months as $value => $month) {
+  if ($selectedMonth-1 == $value) {
+    echo $month;
+  }
+}
