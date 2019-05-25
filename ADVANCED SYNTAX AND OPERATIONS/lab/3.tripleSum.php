fe450dd05a503d43f1267c68f86cd0e9ae992ed5
Georgi Niagulov <goshiniokrump@gmail.com>
2019-05-25 21:37:11 +0300

    triple sum

--- ADVANCED SYNTAX AND OPERATIONS/lab/3.tripleSum.php
+++ ADVANCED SYNTAX AND OPERATIONS/lab/3.tripleSum.php
@@ -1,13 +1,18 @@
 <?php
 
 $integers = array_map("intval", explode(" ", readline()));
-for ($i=0; $i < count($integers)-2; $i++) { 
-  if ($integers[$i]+$integers[$i+1] == $integers[$i+2]) {
-    $a = $integers[$i];
-    $b = $integers[$i+1];
-    $c = $integers[$i+2];
-    printf("%d + %d == %d ".PHP_EOL,$a,$b,$c);
-  } else {
-    echo "no".PHP_EOL;
-  }
+$isSum = false;
+for ($i = 0; $i < count($integers); $i++) {
+    for ($j = $i+1; $j < count($integers); $j++) {
+        for ($k = 0; $k < count($integers); $k++) {
+            if ($integers[$i]+$integers[$j] == $integers[$k]) {
+              echo "$integers[$i] + $integers[$j] == $integers[$k]".PHP_EOL;
+              $isSum = true;
+              break;
+            } 
+        }
+    }
+}
+if ($isSum == false) {
+  echo "No";
 }
